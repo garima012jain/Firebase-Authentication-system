beb831511b991079582a35a5cdfb5a5e248d8499
garima012jain <43791459+garima012jain@users.noreply.github.com>
2020-06-07 08:01:00 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,10 @@ const firebaseConfig = {
   authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
   databaseURL: "https://" + process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseio.com',
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.appspot.com'
+  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.appspot.com',
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 
